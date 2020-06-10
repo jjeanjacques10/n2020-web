acea9f1a5a43589c9e9eeae40c7882e804a7e470
jjeanjacques10 <jjean.jacques10@gmail.com>
2020-06-10 09:00:07 -0300

    Bot controller e jsp

---
+++ src/main/java/br/com/goodbot/n2020/controller/BotController.java
@@ -0,0 +1,20 @@
+package br.com.goodbot.n2020.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/bot")
+public class BotController {
+	@GetMapping()
+	public String findAll() {
+		return "bot/index";
+	}
+}
