bd44aeb2ecaf22efce26c8be90780f4852664c1e
jjeanjacques10 <jjean.jacques10@gmail.com>
2020-06-12 21:45:36 -0300

    Adicionar funcionando

--- src/main/resources/sql/good_bot_dump.sql
+++ src/main/resources/sql/good_bot_dump.sql
@@ -1,4 +1,4 @@
-INSERT INTO BOT( ID, NAME, DEFAULT_ANSWER, DOWNTIME, FAREWELL_MESSAGE,  WELCOME_MESSAGE) values (1, 'Eliza', 'Nao entendi', 1, 'Tcheu', 'Ola, tudo bem?');
+/*INSERT INTO BOT( ID, NAME, DEFAULT_ANSWER, DOWNTIME, FAREWELL_MESSAGE,  WELCOME_MESSAGE) values (1, 'Eliza', 'Nao entendi', 1, 'Tcheu', 'Ola, tudo bem?');
 INSERT INTO BOT( ID, NAME, DEFAULT_ANSWER, DOWNTIME, FAREWELL_MESSAGE,  WELCOME_MESSAGE) values (2, 'Alice', 'Nao entendi', 3, 'Tcheu', 'Ola, tudo bem?');
 INSERT INTO BOT( ID, NAME, DEFAULT_ANSWER, DOWNTIME, FAREWELL_MESSAGE,  WELCOME_MESSAGE) values (3, 'Lucy', 'Nao entendi', 4, 'Tcheu', 'Ola, tudo bem?');
 INSERT INTO BOT( ID, NAME, DEFAULT_ANSWER, DOWNTIME, FAREWELL_MESSAGE,  WELCOME_MESSAGE) values (4, 'Turing', 'Nao entendi', 5, 'Tcheu', 'Ola, tudo bem?');
@@ -8,4 +8,4 @@ INSERT INTO SEGMENTS(id, NAME, ID_BOT) VALUES (1, 'Saude',1);
 INSERT INTO SEGMENTS(id, NAME, ID_BOT) VALUES (2, 'Educacao',2);
 INSERT INTO SEGMENTS(id, NAME, ID_BOT) VALUES (3, 'Animes',3);
 INSERT INTO SEGMENTS(id, NAME, ID_BOT) VALUES (4, 'Estudos',4);
-INSERT INTO SEGMENTS(id, NAME, ID_BOT) VALUES (5, 'Programacao',5);
+INSERT INTO SEGMENTS(id, NAME, ID_BOT) VALUES (5, 'Programacao',5);*/
